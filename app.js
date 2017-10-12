8a8766e54a66c41bc4770c083a9fa6f4650b1c38
Cheiron Zhang <cheironde@gmail.com>
2017-10-12 11:55:07 +0800

    save

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 const Koa = require('koa')
 const Router = require('koa-router')
 const isBot = require('koa-isbot')
@@ -20,20 +22,21 @@ app.listen(3000, () => {
   console.log('listening on port 3000')
 })
 
-const ws = require('koa-websocket')
+const WebSocket = require('ws')
 
-const wsapp = ws(new Koa())
-
-wsapp.ws.use(ctx => {
-  ctx.websocket.on('message', message => {
-    console.log('message received.')
-    ctx.websocket.send('hello client')
-    setInterval(() => {
-      ctx.websocket.send('dong...')
-    }, 1400)
-  })
+const wsapp = new WebSocket.Server({ port: 3001 }, () => {
+  console.log('websocket listening on port 3001')
 })
 
-wsapp.listen(3001, () => {
-  console.log('websocket listen on port 3001')
+wsapp.on('connection', ws => {
+  ws.on('message', message => {
+    console.log('message received.')
+    ws.send('hello client.')
+    const picture = fs.readFileSync(__dirname + '/screenshots/test.png')
+    ws.send(Date.now())
+    ws.send(picture)
+    // setInterval(() => {
+    //   ws.send('dong...')
+    // }, 1400)
+  })
 })
